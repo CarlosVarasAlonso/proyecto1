b4fe59a00d70ec2f899f98ddee58b6fc81c06c59
CarlosVarasAlonso <carlos.varalo@educa.jcyl.es>
2023-04-11 19:23:56 +0200

    Cambios proyecto principal Añadido clase Cambio2

--- src/cambio1/Cambio1.java
+++ src/cambio1/Cambio1.java
@@ -6,5 +6,10 @@ public class Cambio1 {
 		// TODO Auto-generated method stub
 		System.out.println("Pruebas Git. Clase Cambio1");
 	}
-
+	public class Cambio2 {
+		public static void main(String[] args) {
+			System.out.println("Proyecto Inicial de proyecto1");
+			System.out.println("Cambios en el proyecto Inicial");
+		}
+	}
 }
