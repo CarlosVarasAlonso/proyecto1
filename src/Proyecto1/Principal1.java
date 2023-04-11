9fd4a5e0176c1b6a346ef7ab789c8efdae1bc21b
CarlosVarasAlonso <carlos.varalo@educa.jcyl.es>
2023-04-11 18:51:32 +0200

    Cambios clase principal

--- src/Proyecto1/Principal1.java
+++ src/Proyecto1/Principal1.java
@@ -5,6 +5,7 @@ public class Principal1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola git 2.0");
+		System.out.println("Pruebas Git. Se añade cambio1");
 	}
 
 }
